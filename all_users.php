bf80565d2648c63dae53f830f0a1d7d34a893b88
ADRIEN CAUBEL <batman.caubel@laposte.net>
2019-11-13 09:27:55 +0100

    fix #1 Base de données prête

--- all_users.php
+++ all_users.php
@@ -1,22 +1,36 @@
-<?php
-	/* Connexion à la base */
-	$host = 'localhost';
-	$db = 'my_activities';
-	$user = 'root';
-	$pass = 'root';
-	$charset = 'utf8';
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>all users </title>
+	</head>
+	<body>
+	
+	<?php
+		$host = 'localhost';
+		$db   = 'my_activities';
+		$user = 'root';
+		$pass = 'root';
+		$charset = 'utf8mb4';
+		$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+		$options = [
+			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::ATTR_EMULATE_PREPARES   => false,
+		];
+		try {
+			 $pdo = new PDO($dsn, $user, $pass, $options);
+		} catch (PDOException $e) {
+			 throw new PDOException($e->getMessage(), (int)$e->getCode());
+		}
+	?>
 
-	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-	$options = [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES=>false, ];
-				
-	try {
-		$pdo = new PDO($dsn, $user, $pass, $options); 
-	} catch (PDOException $e) {      
-		throw new PDOException($e->getMessage(), (int)$e->getCode());
-	} 
-
-	/* Exécution de la requete */
-
-?>
+	<?php
+		$stmt = $pdo->query('SELECT username FROM users');
+		while ($row = $stmt->fetch())
+		{
+			echo $row['username'] . "\n";
+		}
+	?>
+	</body>
+</html> 
